47cc9384dcaf76d39c1b97a01f61d49be5953923
Vishal Singh <vk24122005@gmail.com>
2025-11-21 20:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (50.45%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
         if(n==1) return true;
-        if(n<0) return false;
+        if(n%2!=0) return false;
+        if(n==0) return false;
+        
 
-        int highestPowerOfTwo= 1 << 30;
-
-        if(highestPowerOfTwo%n == 0) return true;
-        return false;
-
-
+        return isPowerOfTwo(n/2);
     }
 };
