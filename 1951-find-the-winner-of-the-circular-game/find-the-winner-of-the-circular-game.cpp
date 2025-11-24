516211b41497bff6cc1c55002e90b5849caa3fec
Vishal Singh <vk24122005@gmail.com>
2025-11-24 10:26:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (93.46%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int funct(vector<int>& arr, int k, int i) {
-        if (arr.size() == 1)
-            return arr[0];
+    int funct(int n, int k, int i) {
+        // if (arr.size() == 1)
+        //     return arr[0];
 
-        i = (k + i - 1) % arr.size();
-        arr.erase(arr.begin() + i);
-        return funct(arr, k, i);
+        // i = (k + i - 1) % arr.size();
+        // arr.erase(arr.begin() + i);
+        // return funct(arr, k, i);/
+        if(n==1) return i;
+        return (funct(n-1, k, i)+k)%n;
+ 
     }
 
     int findTheWinner(int n, int k) {
-        vector<int> temp;
-        for (int i = 1; i < n + 1; i++)
-            temp.push_back(i);
-        return funct(temp, k, 0);
+        // vector<int> temp;
+        // for (int i = 1; i < n + 1; i++)
+        //     temp.push_back(i);
+        // return funct(temp, k, 0);
+        return funct(n, k, 0)+1;
     }
 };
