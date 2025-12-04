1ea8fd2c9dfc265f2c1d017be62bee13a0bdb6bb
Vishal Singh <vk24122005@gmail.com>
2025-12-04 10:18:21 +0530

    Time: 3 ms (62.96%) | Memory: 8.2 MB (11.57%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -8,14 +8,19 @@ public:
 
     // using Dynamic programming
 
+
+
+    // using  top down approch 
     int fib(int n) {
         // creating an dp array of n+1 size
         // initialising some values to it like -1;
         vector<int> dp(n + 1, -1);
-        if(n==0 || n==1) return n; 
+        if(n==0 || n==1) return n;
+        dp[0]=0, dp[1]=1; 
         solve(dp, n);
         return dp[n];
     }
+/*
 
     int solve(vector<int>& dp, int n) {
 
@@ -28,4 +33,21 @@ public:
         dp[n] = solve(dp, n - 1) + solve(dp, n - 2);
         return dp[n];
     }
+
+*/
+
+
+    //using bottom up approch:-
+
+    int solve(vector<int>& dp, int n){
+        if(n<=1) return n;
+        
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+
+    }
+
+
 };
