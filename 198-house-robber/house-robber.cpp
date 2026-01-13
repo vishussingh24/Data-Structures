f8b34c2e7f0feb3db2a150874825bd6cdc7ac784
Vishal Singh <vk24122005@gmail.com>
2026-01-13 06:38:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (43.92%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
 
-    int solve (int idx, vector<int>& nums, int n, vector<int>& dp){
+    // USING MEMOIZATION -
+
+    int memo (int idx, vector<int>& nums, int n, vector<int>& dp){
         if(idx==0) return nums[idx];
         if(idx<0) return 0;
 
         if(dp[idx] != -1) return dp[idx];
 
-        int pick = nums[idx]+solve(idx-2, nums, n, dp);
-        int skip = 0+solve(idx-1, nums, n, dp);
+        int pick = nums[idx]+memo(idx-2, nums, n, dp);
+        int skip = 0+memo(idx-1, nums, n, dp);
         return dp[idx] = max(pick, skip);
     }
+
+
+    int tabulation(vector<int>& nums, int n, vector<int>& dp){
+
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+            int pick = nums[i];
+            if(i>1){
+                pick+=dp[i-2];
+            }
+            int skip = 0+dp[i-1];
+            dp[i] = max(pick, skip);
+        }
+        return dp[n-1];
+    }
+
 
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, -1);
-        return solve(n-1, nums, n, dp);
+       return tabulation(nums, n ,dp);
     }
+
+
+
+
+
+
 };
